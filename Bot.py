3d564c06f00cf2ee7671d8a9b797cef0159b67c9
Goodmorning-Takaya <goodmorning.takaya@gmail.com>
2022-07-25 23:32:10 -0700

    Fixed variable names for consistency and made driver/action an attribute of the bot

--- Bot.py
+++ Bot.py
@@ -12,11 +12,9 @@ import tkinter as tk
 #TEST ITEM
 
 nikeURL = 'https://www.nike.com/t/mens-basketball-t-shirt-RxlfV9/DV1214-100'
-colorBTNclass = 'nr-pdp-colorway-DV1214-100'
-sizeBTNid = 'skuAndSize__27962581'
-addCartBTNclass = 'ncss-btn-primary-dark btn-lg add-to-cart-btn'
-driver = webdriver.Safari()
-action = ActionChains(driver)
+
+
+
 
 def setupConfig(file: str) -> ConfigParser:
 	print("Parsing Configuration...")
@@ -27,6 +25,10 @@ def setupConfig(file: str) -> ConfigParser:
 class CheckoutBot():
 	'''Bot automates purchasing a product given a product URL, credentials and size/color'''
 	def __init__(self,config, URL, size, color):
+
+		self.driver = webdriver.Safari()
+		self.action = ActionChains(driver)
+
 		self.username = config.get("NIKE_ACCOUNT", "USERNAME")
 		self.password = config.get("NIKE_ACCOUNT", "PASSWORD")
 		self.ID = config.get("NIKE_ACCOUNT", "ID")
@@ -34,65 +36,74 @@ class CheckoutBot():
 		self.ccsecurity = config.get("CREDIT_CARD", "CCV")
 		self.expiration_month = config.get("CREDIT_CARD", "EXPM")
 		self.expiration_year = config.get("CREDIT_CARD", "EXPY")
-		self.URL = driver.get(nikeURL)
-		self.addcartElement = driver.find_element(By.CLASS_NAME, addCartElem)
-		self.colorElement = driver.find_element(By.CLASS_NAME, color)
-		self.sizeElement = driver.find_element(By.ID, size)
+		self.URL = URL
 
 	def login(self) -> None:
 		'''Uses the webdriver to locate the login fields and logs the user in using the passed credentials'''
-		signBTN = 'nav-btn p0-sm d-sm-b body-4 u-bold ml2-sm mr2-sm'
-		logID = '192ffd08-4d60-408c-8c75-821cad9c0cc7'
-		pwID = '3104580a-d23b-43d1-8782-babc5be49fad'
+		driver.get(self.URL)
+		signButtonClass = 'nav-btn p0-sm d-sm-b body-4 u-bold ml2-sm mr2-sm'
+		loginButtonId = '192ffd08-4d60-408c-8c75-821cad9c0cc7'
+		passwordButtonId = '3104580a-d23b-43d1-8782-babc5be49fad'
 		
-		signBTNclick = driver.find_element(BY.CLASS_NAME, signBTN)
-		login = driver.find_element(By.ID, logID)
-		pw = driver.find_element(By.ID, pwID)
+		signInElement = driver.find_element(BY.CLASS_NAME, signButtonClass)
+		loginElement = driver.find_element(By.ID, loginButtonId)
+		passwordElement = driver.find_element(By.ID, passwordButtonId)
 
-		action.move_to_element(signBTNclick)
-		action.click(signBTNclick)
-		action.move_to_element(login)
-		action.click(login)
-		action.send_keys(username)
-		action.move_to_element(pw)
-		action.click(pw)
-		action.send_keys(password)
+		action.move_to_element(signInElement)
+		action.click(signInElement)
+		action.move_to_element(loginElement)
+		action.click(loginElement)
+		action.send_keys(self.username)
+		action.move_to_element(passwordElement)
+		action.click(passwordElement)
+		action.send_keys(self.password)
 		action.perform()
-		pass
+		
 
 	def purchase(self) -> None:
 		'''Execute a purchase'''
-		pass
+		self._selectColor()
+		self._selectSize()
+		self._addToCart()
+		#self._addToCart()
+		#self._checkout()
 
 	def _selectColor(self) -> None:
 		'''Uses the webdriver to select the color of item to be checked out'''
+		colorButtonClass = 'nr-pdp-colorway-DV1214-100'
+		colorElement = driver.find_element(By.CLASS_NAME, colorButtonClass)
 		action.move_to_element(colorElement)
 		action.click(colorElement)
-		pass
+		
 
 	def _selectSize(self) -> None:
 		'''Uses the webdriver to select the size of item to be checked out'''
+		sizeButtonId = 'skuAndSize__27962581'
+		sizeElement = driver.find_element(By.ID, sizeButtonId)
 		driver.move_to_element(sizeElement)
 		driver.click(sizeElement)
-		pass
+		
 
 	def _addToCart(self) -> None:
 		'''Uses the webdriver to create and execute an action chain to reach the checkout page'''
+		addCartClass = 'ncss-btn-primary-dark btn-lg add-to-cart-btn'
+		addcartElement = driver.find_element(By.CLASS_NAME, addCartClass)
 		action.move_to_element(addcartElement)
 		action.click(addcartElement)
-		pass
+		
 
 	def _checkout(self) -> None:
 		'''Uses the webdriver to complete checkout of order'''
-		checkoutCLASS = 'ncss-btn-primary-dark btn-lg mr3-sm css-1n4ymyz'
-		checkoutBTN = driver.find_element(By.CLASS_NAME, checkoutCLASS)
-		action.move_to_element(checkoutBTN)
-		action.click(checkoutBTN)
-                #IF Nike Account, all info should be filled, just need to click 'Place Order'
+		checkoutClass = 'ncss-btn-primary-dark btn-lg mr3-sm css-1n4ymyz'
+		checkoutButtonElement = driver.find_element(By.CLASS_NAME, checkoutClass)
+		action.move_to_element(checkoutButtonElement)
+		action.click(checkoutButtonElement)
+        #IF Nike Account, all info should be filled, just need to click 'Place Order'
 		placeorderID = ''
 		placeorderBTN = driver.find_element(By.ID, placeorderID)
 		action.move_to_element(placeorderBTN)
 		action.click(placeorderBTN)
+		#self._enterCard()
 		pass
 	
 	def _enterCard(self) -> None:
